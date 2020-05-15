7967784b452409f3296db0fe5697972013f7d89b
sp-rush <63054319+sp-rush@users.noreply.github.com>
2020-05-15 21:10:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,7 +4,21 @@ import traceback
 
 bot = commands.Bot(command_prefix='キョウカちゃん、')
 token = os.environ['DISCORD_BOT_TOKEN']
-        
+
+RoundCount = 0 # 周回数
+StageCount = 0 # 段階数
+DateCount = 0 # 日数
+DateCountLast = 0 # 最終日
+BossNum = ["1","2","3","4","5"] # ボス番号
+BossList = ["0","ミノタウロス","トライロッカー","メガラパーン","ワイルドグリフォン","ゴブリングレート"] # ボス名前
+BossHP = 0 # ボスHP
+MemberList = [] #メンバーリスト
+Booking1 = [] # 予約を追加するリスト
+Booking2 = [] # book→予約
+Booking3 = []
+Booking4 = []
+Booking5 = []
+
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
@@ -33,7 +47,7 @@ async def 予約確認(ctx, a: int):
                 member = ""
             for one in tmpList:
                 member += one + " "
-            await ctx.send(BossName + ":" + member)
+    await ctx.send(BossName + ":" + member)
 
 @bot.command()
 async def ping(ctx):
