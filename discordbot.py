669cdf7a86fee140fa8c3fb8c001ae3b062d9a9c
sp-rush <63054319+sp-rush@users.noreply.github.com>
2020-05-15 21:06:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,6 +17,25 @@ async def 足し算(ctx, a: int, b: int):
     await ctx.send(str(a) + '+' + str(b) + 'は' + str(ans) + 'です')
 
 @bot.command()
+async def 引き算(ctx, a: int, b: int):
+    ans = a - b
+    await ctx.send(str(a) + '-' + str(b) + 'は' + str(ans) + 'です')
+
+@bot.command()
+async def 予約確認(ctx, a: int):
+    await ctx.send('予約を確認します。')
+    for Boss in BossNum:
+        tmpList = []
+            if Boss in a:
+                BookList = "Booking" + Boss
+                tmpList = [x[0] for x in eval(BookList)]
+                BossName = BossList[int(Boss)]
+                member = ""
+            for one in tmpList:
+                member += one + " "
+            await ctx.send(BossName + ":" + member)
+
+@bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
